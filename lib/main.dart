d9a9847d3ff9503c7cd649beb364d5d881091e80
Krzysztof Adamowicz <krzysztof.adamowicz@softcream.pl>
2020-01-30 07:46:40 +0100

    TeX

--- lib/main.dart
+++ lib/main.dart
@@ -6,22 +6,23 @@ class Figure {
   final int code;
   final String title;
   final String description;
+  final String texCode;
   final String image;
   final bool aShow;
   final bool bShow;
   final bool hShow;
 
-  Figure(this.code, this.title, this.description, this.image, this.aShow,
-      this.bShow, this.hShow);
+  Figure(this.code, this.title, this.description, this.texCode, this.image,
+      this.aShow, this.bShow, this.hShow);
 }
 
 class Parameters {
   int code;
-  double a=0;
-  double b=0;
-  double h=0;
-  double d1=0;
-  double d2=0;
+  double a = 0;
+  double b = 0;
+  double h = 0;
+  double d1 = 0;
+  double d2 = 0;
 
   double calcArea() {
     switch (code) {
@@ -60,22 +61,22 @@ void main() {
 
 class HomeScreen extends StatelessWidget {
   final List<Figure> figures = [
-    Figure(1, 'Trójkąt', 'Pole trójkąta', 'images/Triangle.png', true, false,
+    Figure(1, 'Trójkąt', 'Pole trójkąta', r'\frac{1}{2}ah', 'images/Triangle.png', true, false,
         true),
-    Figure(2, 'Trójkąt równoboczny', 'Pole trójkąta',
+    Figure(2, 'Trójkąt równoboczny', 'Pole trójkąta', r'\frac{1}{2}ah',
         'images/Triangle_isosceles.png', true, false, true),
-    Figure(3, 'Trójkąt prostokątny', 'Pole trójkąta',
+    Figure(3, 'Trójkąt prostokątny', 'Pole trójkąta', r'\frac{1}{2}ah',
         'images/Triangle_rectangular.png', true, false, true),
-    Figure(4, 'Prostokąt', 'Pole prostokąta', 'images/Rectangle.png', true,
+    Figure(4, 'Prostokąt', 'Pole prostokąta', r'\frac{1}{2}ah', 'images/Rectangle.png', true,
         true, false),
     Figure(
-        5, 'Kwadrat', 'Pole kwadratu', 'images/Square.png', true, false, false),
-    Figure(6, 'Równoległobok', 'Pole równoległoboku',
+        5, 'Kwadrat', 'Pole kwadratu', r'a^2', 'images/Square.png', true, false, false),
+    Figure(6, 'Równoległobok', 'Pole równoległoboku', r'ah',
         'images/Parallelogram.png', true, false, true),
-    Figure(7, 'Romb', 'Pole rombu', 'images/Diamond.png', true, false, true),
+    Figure(7, 'Romb', 'Pole rombu', r'ah', 'images/Diamond.png', true, false, true),
     Figure(
-        8, 'Trapez', 'Pole trapezu', 'images/Trapezoid.png', true, true, true),
-    Figure(9, 'Trapez równoramienny', 'Pole trapezu',
+        8, 'Trapez', 'Pole trapezu', r'\frac{a+b}{2}h', 'images/Trapezoid.png', true, true, true),
+    Figure(9, 'Trapez równoramienny', 'Pole trapezu', r'\frac{a+b}{2}h',
         'images/Trapezoid_isosceles.png', true, true, true),
   ];
 
@@ -120,6 +121,8 @@ class _DetailsState extends State<Details> {
   final GlobalKey<FormState> _formStateKey = GlobalKey<FormState>();
   Parameters param = Parameters();
   String answer = '';
+  String desc = '';
+  bool resultVisible = false;
 
   String _validateNumber(String value) {
     double _valueAsInteger = value.isEmpty ? 0 : double.tryParse(value);
@@ -130,7 +133,8 @@ class _DetailsState extends State<Details> {
     if (_formStateKey.currentState.validate()) {
       _formStateKey.currentState.save();
       setState(() {
-        answer = 'Pole ' + param.calcArea().toString();
+        resultVisible = true;
+        answer = param.calcArea().toString();
       });
     }
   }
@@ -139,6 +143,7 @@ class _DetailsState extends State<Details> {
   Widget build(BuildContext context) {
     final Figure figure = ModalRoute.of(context).settings.arguments;
     param..code = figure.code;
+    desc = figure.description+"="+figure.texCode;
 
     var bar = AppBar(
       title: Text(figure.title),
@@ -163,71 +168,58 @@ class _DetailsState extends State<Details> {
             key: _formStateKey,
             autovalidate: true,
             child: Padding(
-              padding: EdgeInsets.all(16.0 ),
+              padding: EdgeInsets.all(16.0),
               child: Column(
                 children: <Widget>[
                   Visibility(
                     visible: figure.aShow,
-                      child: TextFormField(
-                        decoration: InputDecoration(
-                          hintText: 'Podaj bok a',
-                          labelText: 'a',
-                          border: OutlineInputBorder(),
-                        ),
-                        validator: (value) => _validateNumber(value),
-                        onSaved: (value) => param.a = double.tryParse(value),
-                        keyboardType: TextInputType.number,
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        hintText: 'Podaj bok a',
+                        labelText: 'a',
+                        border: OutlineInputBorder(),
                       ),
+                      validator: (value) => _validateNumber(value),
+                      onSaved: (value) => param.a = double.tryParse(value),
+                      keyboardType: TextInputType.number,
+                    ),
                   ),
                   Visibility(
-                      visible: figure.bShow,
-                      child: TextFormField(
-                        decoration: InputDecoration(
-                          hintText: 'Podaj bok b',
-                          labelText: 'b',
-                          border: OutlineInputBorder(),
-                        ),
-                        validator: (value) => _validateNumber(value),
-                        onSaved: (value) => param.b = double.tryParse(value),
-                        keyboardType: TextInputType.number,
-
+                    visible: figure.bShow,
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        hintText: 'Podaj bok b',
+                        labelText: 'b',
+                        border: OutlineInputBorder(),
                       ),
+                      validator: (value) => _validateNumber(value),
+                      onSaved: (value) => param.b = double.tryParse(value),
+                      keyboardType: TextInputType.number,
+                    ),
                   ),
                   Visibility(
-                      visible: figure.hShow,
-                      child: TextFormField(
-                        decoration: InputDecoration(
-                          hintText: 'Podaj wysokość',
-                          labelText: 'h',
-                          border: OutlineInputBorder(),
-                        ),
-                        validator: (value) => _validateNumber(value),
-                        onSaved: (value) => param.h = double.tryParse(value),
-                        keyboardType: TextInputType.number,
+                    visible: figure.hShow,
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        hintText: 'Podaj wysokość',
+                        labelText: 'h',
+                        border: OutlineInputBorder(),
                       ),
+                      validator: (value) => _validateNumber(value),
+                      onSaved: (value) => param.h = double.tryParse(value),
+                      keyboardType: TextInputType.number,
+                    ),
                   ),
                   RaisedButton(
                     onPressed: () => _submitOrder(),
                     child: Text('Oblicz pole'),
                     color: Colors.lightGreen,
                   ),
-            Container(
-              margin: EdgeInsets.all(10),
-              child: Card(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TeXView(
-                    teXHTML: r"""$$Pole trójkąta = \frac{1}{2}ah$$""",
-                  ),
-                ),
-              ),
-            ),                  
-                  Text(
-                    answer,
-                    style: TextStyle(
-                      fontWeight: FontWeight.w800,
-                      letterSpacing: 0.5,
-                      fontSize: 20,
+                  Visibility(
+                    visible: resultVisible,
+                    child: TeXView(
+                      //teXHTML: r"$$Pole\ trójkąta = \frac{1}{2}ah"+"=$answer"+r"$$",
+                      teXHTML: r"$$"+"$desc = $answer"+r"$$",
                     ),
                   ),
                 ],
@@ -241,12 +233,11 @@ class _DetailsState extends State<Details> {
     return Scaffold(
       //resizeToAvoidBottomInset: false,
       appBar: bar,
-      body: SingleChildScrollView (
-        child: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: titleSection,
-        )
-      ),
+      body: SingleChildScrollView(
+          child: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: titleSection,
+      )),
       //body: Padding(
       //  padding: EdgeInsets.all(16.0),
       //  child: titleSection,
